54087e8e495d3e842d5b611654d25a181981baef
Shageesha <shageeshainfo@gmail.com>
2018-10-06 18:45:02 +0530

    Fixed formI6,FORM I3

--- frontend/src/student_main/formi6.jsx
+++ frontend/src/student_main/formi6.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import iconn from "./images/icon.png";
+
 
 export default class Formithree extends React.Component {
     render() {
+
+        const btn_form6 = {
+            height:100,
+            width:100
+        };
 
 
         return (
@@ -14,9 +21,12 @@ export default class Formithree extends React.Component {
                 <form>
                     <div className="container">
 
-                        <h5> Internship Information </h5><br/>
-                        <a href='../doc/Form I-6.pdf' download>Download Industrial Report</a>
-
+                        <h5 className="form-label"> Internship Information </h5><br/>
+                    
+                        <a href='../doc/Form I-6.pdf' className="form-6-download" download>Download Industrial Report</a>
+                        <br />
+                        <img href='../doc/Form I-6.pdf' className="form6-btn" src={iconn} alt="" style={btn_form6}/>
+                        
                     </div>
                 </form>
             </div>
